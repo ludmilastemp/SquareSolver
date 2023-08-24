04c8a6436fa9c8916d8657d9534696523371a604
ludmilastemp <138589010+ludmilastemp@users.noreply.github.com>
2023-08-24 18:04:23 +0300

    Add files via upload

--- tests.cpp
+++ tests.cpp
@@ -1,12 +1,9 @@
-#include "header.h"
 #include "io.h"
 #include "solve.h"
 #include "tests.h"
 
 #include <stdio.h>
 #include <math.h>
-
-bool CheckEquality(double x1, double x2);
 
 bool CheckEquality(double x1, double x2)
 {
@@ -19,6 +16,12 @@ void Test()
     int tests_passed = 0;
     int tests_all = 0;
 
+    const char color_red [] =       "\033[41;1;4m";
+    const char color_green [] =     "\033[102;1;4m";
+    const char color_red_1 [] =     "\033[41;1m";
+    const char color_yellow_1 [] =  "\033[30;103;1m";
+    const char cleaner [] =         "\033[0m";
+
     FILE *fp = fopen("tests.txt", "r");
 
     struct Coeffs coeffs = {};
@@ -30,29 +33,37 @@ void Test()
     {
         ++tests_all;
 
+        roots.x1 = 0;
+        roots.x2 = 0;
+
         fscanf(fp, "%lg", &coeffs.a);
         fscanf(fp, "%lg", &coeffs.b);
         fscanf(fp, "%lg", &coeffs.c);
         fscanf(fp, "%lg", &rootsRef.x1);
         fscanf(fp, "%lg", &rootsRef.x2);
         fscanf(fp, "%d", &count_rootsRef);
+        //fscanf(fp, "%d", &rootsRef.count_roots);
 
         SolveQuadraticEquation(&coeffs, &roots);
 
         if( CheckEquality(roots.x1, rootsRef.x1) &&
             CheckEquality(roots.x2, rootsRef.x2) &&
             CheckEquality(count_rootsRef, roots.count_roots) )
+            //CheckEquality(rootsRef.count_roots, roots.count_roots) )
         {
             ++tests_passed;
+            printf("%s" "\nTest %2d OK" "%s", color_green, tests_all, cleaner);
+
         }
         else
         {
-            printf("ERROR!   x1 = %lg, x2 = %lg, count_roots = %d\n", roots.x1, roots.x2, roots.count_roots);
-            printf("expected x1 = %lg, x2 = %lg, count_roots = %d\n", rootsRef.x1, rootsRef.x2, count_rootsRef);
+            printf("%s" "\nTest %2d !ERROR!" "%s", color_red, tests_all, cleaner);
+            printf("%s" "   x1 = %lg, x2 = %lg, count_roots = %d" "%s", color_red_1, roots.x1, roots.x2, roots.count_roots, cleaner);
+            printf("%s\n        " "expected" "%s", color_red_1, cleaner);
+            printf("%s" "  x1 = %lg, x2 = %lg, count_roots = %d" "%s", color_red_1, rootsRef.x1, rootsRef.x2, count_rootsRef, cleaner);
         }
     }
 
-    printf("\nPassed %d tests out of %d!\n", tests_passed, tests_all);
-    //const char[] color_red = "";
-    //printf("\dfsdfsd" "mHello\033[0m");
+    printf("\n%s" "\nPassed %d tests out of %d!" "%s\n", color_yellow_1, tests_passed, tests_all, cleaner);
+
 }
--- tests.h
+++ tests.h
@@ -1,6 +1,20 @@
 #ifndef __TESTS__
 #define __TESTS__
 
+/**
+    \brief Function compares two floating point numbers
+    \param x1, x2 numbers to compare
+    \return 0 - if the numbers are not equal
+        1 - if the numbers are equal
+*/
+bool CheckEquality(double x1, double x2);
+
+/**
+    \brief Function checks whether the SolveQuadraticEquation function is working correctly
+    \details Function compares the reference roots of several quadratic equations with the roots of the equation obtained using the SolveQuadraticEquation function
+    \param void
+    \return void
+*/
 void Test();
 
 #endif
